a4282b1a70f858c52f0fd355dd80286d3e012c5a
pravallikareddychegireddy <chegireddypravallikareddy@gmail.com>
2025-11-21 15:12:48 +0530

    Update axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://nss-bfjo.onrender.com',
   headers: {
     'Content-Type': 'application/json'
   }
